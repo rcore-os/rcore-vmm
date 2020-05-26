04f8ca2315b5a3b286080f711b1b3bc74c7acab9
chenyu <believe.chenyu@gmail.com>
2020-05-26 21:54:04 +0800

    impl i8250 serial port

--- dev/serial.c
+++ dev/serial.c
@@ -9,16 +9,83 @@
 #define SERIAL_BASE3 0x2e8
 #define SERIAL_SIZE 0x8
 
+#define RECEIVE             0x0
+#define TRANSMIT            0x0
+#define INTERRUPT_ENABLE    0x1
+#define INTERRUPT_ID        0x2
+#define LINE_CONTROL        0x3
+#define MODEM_CONTROL       0x4
+#define LINE_STATUS         0x5
+#define MODEM_STATUS        0x6
+#define SCRATCH             0x7
+
+#define kI8250LineStatusEmpty (1u << 5)
+#define kI8250LineStatusIdle (1u << 6)
+
 struct serial_data {
+    uint8_t interrupt_enable;
+    uint8_t line_control;
 };
 
 static int serial_read(struct virt_device *dev, uint64_t port, struct rvm_io_value *value) {
     printf("serial read handler\n");
+    value->u32 = 0;
+    switch (port - SERIAL_BASE0) {
+        case INTERRUPT_ENABLE:
+            value->access_size = 1;
+            value->u8 = ((struct serial_data*)dev->priv_data)->interrupt_enable;
+            return 0;
+        case LINE_CONTROL:
+            value->access_size = 1;
+            value->u8 = ((struct serial_data*)dev->priv_data)->line_control;
+            return 0;
+        case LINE_STATUS:
+            value->access_size = 1;
+            value->u8 = kI8250LineStatusIdle | kI8250LineStatusEmpty;
+            return 0;
+        case RECEIVE:
+        case INTERRUPT_ID:
+        case MODEM_CONTROL:
+        case MODEM_STATUS... SCRATCH:
+            value->access_size = 1;
+            value->u8 = 0; // FIXME: modify here to read from stdin
+            return 0;
+        default:
+            printf("[ERROR] unhandled I8250 read 0x%x", port);
+            return 1;
+    }
     return 1;
 }
 
 static int serial_write(struct virt_device *dev, uint64_t port, struct rvm_io_value *value) {
-    printf("serial write handler\n");
+    printf("serial write handler 0x%x: %d\n", port, value->access_size);
+    switch (port - SERIAL_BASE0) {
+        case TRANSMIT:
+            for (int i = 0; i < value->access_size; i++) {
+                printf("%c", value->buf[i]);
+            }
+            return 0;
+        case INTERRUPT_ENABLE:
+            if (value->access_size != 1) {
+                // return ZX_ERR_IO_DATA_INTEGRITY;
+                return 1;
+            }
+            ((struct serial_data*)dev->priv_data)->interrupt_enable = value->u8;
+            return 0;
+        case LINE_CONTROL:
+            if (value->access_size != 1) {
+                // return ZX_ERR_IO_DATA_INTEGRITY;
+                return 1;
+            }
+            ((struct serial_data*)dev->priv_data)->line_control = value->u8;
+            return 0;
+        case INTERRUPT_ID:
+        case MODEM_CONTROL... SCRATCH:
+            return 0;
+        default:
+            printf("[ERROR] unhandled I8250 write 0x%x", port);
+            return 1;
+    }
     return 1;
 }
 
